d0ff41ee528559f1541543b50cb690124febcbf5
krisped <81587180+krisped@users.noreply.github.com>
2025-08-16 21:33:26 +0200

    update
    update

--- src/main/java/com/krisped/KPWebhookPlugin.java
+++ src/main/java/com/krisped/KPWebhookPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.krisped;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class KPWebhookPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private KPWebhookConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    KPWebhookConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(KPWebhookConfig.class);
 	}
 }
